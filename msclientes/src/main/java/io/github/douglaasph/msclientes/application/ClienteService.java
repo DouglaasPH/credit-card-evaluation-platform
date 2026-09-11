8936b618da9ebc1996178401476a4f7e7d7a2bb1
Douglas Phelipe <douglasphelipedpss@gmail.com>
2026-09-11 00:45:48 -0300

    implementing the customer microservice

---
+++ msclientes/src/main/java/io/github/douglaasph/msclientes/application/ClienteService.java
@@ -0,0 +1,24 @@
+package io.github.douglaasph.msclientes.application;
+
+import io.github.douglaasph.msclientes.domain.Cliente;
+import io.github.douglaasph.msclientes.infra.repository.ClienteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ClienteService {
+    private final ClienteRepository repository;
+
+    @Transactional
+    public Cliente save(Cliente cliente) {
+        return repository.save(cliente);
+    }
+
+    public Optional<Cliente> getByCPF(String cpf) {
+        return repository.findByCpf(cpf);
+    }
+}
