7f691cdbc9bed70f8b54456681165a241cf54f8c
Douglas Phelipe <douglasphelipedpss@gmail.com>
2026-09-11 17:57:58 -0300

    implementing the avaliador credito microsservice 1/2

---
+++ msavaliadorcredito/src/main/java/io/github/douglaasph/msavaliadorcredito/application/ex/DadosClienteNotFoundException.java
@@ -0,0 +1,7 @@
+package io.github.douglaasph.msavaliadorcredito.application.ex;
+
+public class DadosClienteNotFoundException extends Exception {
+    public DadosClienteNotFoundException() {
+        super("Dados do cliente não encontrados para o CPF informado.");
+    }
+}
