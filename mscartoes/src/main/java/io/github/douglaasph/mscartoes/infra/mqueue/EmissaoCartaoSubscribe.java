84e3cc42f5923ea487e44c5fcfefe300b0eac98e
Douglas Phelipe <douglasphelipedpss@gmail.com>
2026-09-12 15:42:19 -0300

    creating the subscriber for the card issuance queue

---
+++ mscartoes/src/main/java/io/github/douglaasph/mscartoes/infra/mqueue/EmissaoCartaoSubscribe.java
@@ -0,0 +1,13 @@
+package io.github.douglaasph.mscartoes.infra.mqueue;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmissaoCartaoSubscribe {
+    @RabbitListener(queues = "${mq.queues.emissao-cartoes}")
+    public void receberSolicitacaoEmissao(@Payload String payload) {
+        System.out.println(payload);
+    }
+}
